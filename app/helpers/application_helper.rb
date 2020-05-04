b2c75b61e196deb1238a8d8225c3a696e17b3e46
Suyesh Bhandari <suyeshb@gmail.com>
2020-05-03 23:51:42 -0400

    Add title functionality for project show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts << 'Ticketee').join(' - ')
+      end
+    end
+  end
 end
--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -12,5 +12,11 @@ RSpec.feature 'Users can create new projects' do
     click_button 'Create Project'
 
     expect(page).to have_content 'Project has been created'
+
+    project = Project.find_by(name: 'Visual Studio Code')
+    expect(page.current_url).to eq project_url(project)
+
+    title = "#{project.name} - Projects - Ticketee"
+    expect(page).to have_title title
   end
 end
